00443b705d1ac740dc3b247e8b3d7207c8b239ba
Maxim <machsim26@gmail.com>
2022-05-25 07:53:21 +0500

    3

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -9,12 +9,12 @@ public class Task05Main {
 
     public static void main(String[] args) {
 
-        /*
+
 
         // Random variables
-        String randomFrom = "..."; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
-        String randomTo = "...";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
-        int randomSalary = 100;  // Некоторое случайное целое положительное число. Можете выбрать его самостоятельно.
+        String randomFrom = "hello"; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        String randomTo = "qwerty";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        int randomSalary = 2;  // Некоторое случайное целое положительное число. Можете выбрать его самостоятельно.
 
         // Создание списка из трех почтовых сообщений.
         MailMessage firstMessage = new MailMessage(
@@ -86,10 +86,6 @@ public class Task05Main {
         assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)) : "wrong salaries mailbox content (1)";
         assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)) : "wrong salaries mailbox content (2)";
         assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
-
-
-        */
-
     }
 
 }
